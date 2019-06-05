205c325074caa9dddc25996495c92e1ef99ae6d7
ollinieminen <otn987@gmail.com>
2019-06-05 12:25:20 -0700

    push everything to git

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // this always comes first
-// require('./lib/utils/connect')();
+require('./lib/utils/connect')();
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
